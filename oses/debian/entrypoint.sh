79435f3bcf5a9c0bd30c7720c15e2957b697b9c3
Batuhan Aydemir <batuaydemir.edu@gmail.com>
2025-12-13 16:48:03 +0300

    feat: Node.js 24 eklendi ve GitHub Actions workflow'ları oluşturuldu (ghcr.io publish)

--- oses/debian/entrypoint.sh
+++ oses/debian/entrypoint.sh
@@ -1,26 +1,4 @@
-#!/bin/bash
-#
-# Copyright (c) 2021 Matthew Penner
-# Copyright (c) 2024 Rustopic
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+
 
 # Default the TZ environment variable to UTC.
 TZ=${TZ:-UTC}
@@ -40,6 +18,6 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@runtimes~ \033[0m%s\n" "$PARSED"
+printf "\033[1m\033[33mrustopic@server~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
